16704aa22e56ff3582b3038059f512eaac02feb5
Jens Fischer <soth@gmx.net>
2015-01-31 00:11:10 +0100

    spring boot project skeleton

---
+++ src/main/java/plt/PartyLootTrackerApplication.java
@@ -0,0 +1,12 @@
+package plt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyLootTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyLootTrackerApplication.class, args);
+    }
+}
